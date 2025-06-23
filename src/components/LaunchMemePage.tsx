468c09a5696e4291205b0be73c09c2aeebee1c44
ketosky-eth <ketoskygaming@gmail.com>
2025-06-24 02:28:17 +0800

    Updated LaunchMemePage.tsx

--- src/components/LaunchMemePage.tsx
+++ src/components/LaunchMemePage.tsx
@@ -260,7 +260,7 @@ export default function LaunchMemePage() {
                         type="text"
                         value={tokenData.initialBuyAmount}
                         onChange={(e) => handleInputChange('initialBuyAmount', e.target.value)}
-                        placeholder="0.1"
+                        placeholder="0.001"
                         className="w-full px-4 py-3 bg-gray-700 border border-gray-600 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
                       />
                     </div>
