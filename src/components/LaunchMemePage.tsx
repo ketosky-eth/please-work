4166ecf40a50a760f5b630130c75ec5d7eb8b3ab
ketosky-eth <ketoskygaming@gmail.com>
2025-06-24 02:20:12 +0800

    Updated LaunchMemePage.tsx

--- src/components/LaunchMemePage.tsx
+++ src/components/LaunchMemePage.tsx
@@ -356,7 +356,7 @@ export default function LaunchMemePage() {
                 </>
               )}
             </button>
-            <p><span className="text-m text-yellow-500 text-center">Warning:</span></p>
+            <p className="text-m text-yellow-500 text-center">Warning:</p>
             <p className="text-s text-gray-500 text-center">
               Launching without a Smart Vault will not earn you liquidity rewards post-bonding curve.
             </p>
