289ea3fc5175e05d71d6e7743e54437850e9abcf
ketosky-eth <ketoskygaming@gmail.com>
2025-07-03 05:31:34 +0800

    Updated RoadmapPage.tsx

--- src/components/RoadmapPage.tsx
+++ src/components/RoadmapPage.tsx
@@ -236,7 +236,6 @@ export default function RoadmapPage() {
                   className="w-full h-full object-cover"
                 />
               </div>
-              <span className="text-lg font-bold text-white">VYTO Protocol</span>
               <span className="text-gray-400 text-sm hidden md:inline">
                 © VYTO Protocol - Powered by the Ronin Network
               </span>
