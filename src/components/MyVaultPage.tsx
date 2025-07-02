9db32fc9a0d0beb012b5a92466f916de9ed34bc1
ketosky-eth <ketoskygaming@gmail.com>
2025-07-03 05:31:02 +0800

    Updated MyVaultPage.tsx

--- src/components/MyVaultPage.tsx
+++ src/components/MyVaultPage.tsx
@@ -235,7 +235,6 @@ export default function MyVaultPage() {
                   className="w-full h-full object-cover"
                 />
               </div>
-              <span className="text-lg font-bold text-white">VYTO Protocol</span>
               <span className="text-gray-400 text-sm hidden md:inline">
                 © VYTO Protocol - Powered by the Ronin Network
               </span>
