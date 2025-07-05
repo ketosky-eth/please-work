59416b8586faa33e54184de48ae79e709222a053
ketosky-eth <ketoskygaming@gmail.com>
2025-07-05 15:32:20 +0800

    Updated AllTokensPage.tsx

--- src/components/AllTokensPage.tsx
+++ src/components/AllTokensPage.tsx
@@ -199,7 +199,7 @@ export default function AllTokensPage() {
                 />
               </div>
               <span className="text-gray-400 text-sm">
-                © VYTO Protocol 2025 - All Rights Reserved
+                © VYTO Protocol 2025 - All Rights Reserved.
               </span>
             </div>
 
