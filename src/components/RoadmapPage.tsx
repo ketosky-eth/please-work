4e6ca61616577fdbad3e1084708978dc97815a46
ketosky-eth <ketoskygaming@gmail.com>
2025-07-05 15:32:50 +0800

    Updated RoadmapPage.tsx

--- src/components/RoadmapPage.tsx
+++ src/components/RoadmapPage.tsx
@@ -240,7 +240,7 @@ export default function RoadmapPage() {
                 />
               </div>
               <span className="text-gray-400 text-sm">
-                © VYTO Protocol 2025 - All Rights Reserved
+                © VYTO Protocol 2025 - All Rights Reserved.
               </span>
             </div>
 
