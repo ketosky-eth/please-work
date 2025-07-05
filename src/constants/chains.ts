64f49a349d324bbb21a514116e66cc2c7961e4d5
ketosky-eth <ketoskygaming@gmail.com>
2025-07-05 15:57:29 +0800

    Updated chains.ts

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -3,7 +3,7 @@ import { Chain } from '../types';
 export const SUPPORTED_CHAINS: Chain[] = [
   {
     id: '2021',
-    name: 'Ronin Testnet',
+    name: 'Saigon Testnet',
     symbol: 'RON',
     rpcUrl: 'https://site1.moralis-nodes.com/ronin-testnet/22d6b97153ed4427b60914f349b2336c',
     blockExplorer: 'https://saigon-explorer.roninchain.com'
